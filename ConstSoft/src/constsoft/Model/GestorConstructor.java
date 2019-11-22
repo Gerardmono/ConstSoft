57f78f69f4c5e4a90245f7043a5ba652b32e4e88
GuillermoAugusto <GuillermoAugusto@Augusto_aries>
2019-11-22 11:56:38 -0600

    se añadio codigo implementado

--- ConstSoft/src/constsoft/Model/GestorConstructor.java
+++ ConstSoft/src/constsoft/Model/GestorConstructor.java
@@ -5,10 +5,49 @@
  */
 package constsoft.Model;
 
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import javax.swing.*;
+
 /**
  *
- * @author Gerardo Ramirez
+ * @author GuillermoAugusto
  */
 public class GestorConstructor {
     
+    public GestorConstructor(){
+        
+    }
+    
+    public void insertarConst(JTextField texto1, JTextField texto2,JTextField texto3,JTextField texto4, JPasswordField pass1){
+        String name, telefono,direccion,correo, contraseña;
+        name=texto1.getText();
+        telefono = texto2.getText();
+        direccion = texto3.getText();
+        correo = texto4.getText();
+        contraseña = pass1.getText();
+        insertarConstructor(name, telefono, direccion, correo, contraseña);
+    }
+    
+    public void insertarConstructor(String name,String telefono,String direccion,String correo,String pass){
+         MySQLDB.conectar();
+         int decision;
+         Statement st = MySQLDB.conexion();
+         String query;
+         query="begin";
+         MySQLDB.consultaActualiza(st, query);
+         query="insert into constructores values('0','"+name+"','"+telefono+"','"+direccion+"','"+correo+"','"+pass+"')";
+         MySQLDB.consultaActualiza(st, query);
+         decision=JOptionPane.showConfirmDialog(null,"Desea guardar los datos los datos");
+         if(decision==0){
+             query="commit;";
+             JOptionPane.showMessageDialog(null,"Datos guardados");
+         }else{
+             query="rollback";
+             JOptionPane.showMessageDialog(null,"Datos no guardados");
+         }
+         MySQLDB.consultaActualiza(st, query);
+         MySQLDB.cerrar(st);
+         
+    }
 }
---
+++ ConstSoft/src/constsoft/Model/MySQLDB.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constsoft.Model;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author GuillermoAugusto
+ */
+public class MySQLDB {
+    
+    public static Connection conexion=null; 
+    private static String bd = "consoft"; // Nombre de BD.
+    public static final String user="root";
+    public static final String password="";
+    private static String driver = "com.mysql.jdbc.Driver";
+    // Ruta del servidor.
+    private static String server = "jdbc:mysql://localhost/" + bd;
+    public static void conectar() {
+        try {
+            Class.forName(driver);
+            conexion = DriverManager.getConnection(server,user,password);
+        } catch (Exception e) {
+            System.out.println("Error: Imposible realizar la conexion a BD.");
+            e.printStackTrace();
+        }
+    }
+ 
+    /**
+     * Método para establecer la conexión con la base de datos.
+     *
+     * @return
+     */
+    public static Statement conexion() {
+        Statement st = null;
+        try {
+            st = conexion.createStatement();
+        } catch (SQLException e) {
+            System.out.println("Error: Conexión incorrecta.");
+            //e.printStackTrace();
+        }
+        return st;
+    }
+ 
+    /**
+     * Método para realizar consultas del tipo: SELECT * FROM tabla WHERE..."
+     *
+     * @param st
+     * @param cadena La consulta en concreto
+     * @return
+     */
+    public static ResultSet consultaQuery(Statement st, String cadena) {
+        ResultSet rs = null;
+        try {
+            rs = st.executeQuery(cadena);
+        } catch (SQLException e) {
+            System.out.println("Error con: " + cadena);
+            System.out.println("SQLException: " + e.getMessage());
+            //e.printStackTrace();
+        }
+        return rs;
+    }
+ 
+    /**
+     * Método para realizar consultas de actualización, creación o eliminación.
+     *
+     * @param st
+     * @param cadena La consulta en concreto
+     * @return
+     */
+    public static int consultaActualiza(Statement st, String cadena) {
+        int rs = -1;
+        try {
+            rs = st.executeUpdate(cadena);
+        } catch (SQLException e) {
+            System.out.println("Error con: " + cadena);
+            System.out.println("SQLException: " + e.getMessage());
+            //e.printStackTrace();
+        }
+        return rs;
+    }
+ 
+    /**
+     * Método para cerrar la consula
+     *
+     * @param rs
+     */
+    public static void cerrar1(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (Exception e) {
+                System.out.print("Error: No es posible cerrar la consulta.");
+            }
+        }
+    }
+ 
+    /**
+     * Método para cerrar la conexión.
+     *
+     * @param st
+     */
+    public static void cerrar(java.sql.Statement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (Exception e) {
+                System.out.print("Error: No es posible cerrar la conexión.");
+            }
+        }
+    }
+     
+   
+}
